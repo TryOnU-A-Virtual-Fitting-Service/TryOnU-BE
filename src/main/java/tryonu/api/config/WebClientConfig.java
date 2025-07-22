c4c9dd4a456c31c27fadef77e42cfe53711060b9
givemethatsewon <nvpz1598@gmail.com>
2025-07-23 03:19:57 +0900

    feat: WebClient에 요청 및 응답 로깅 기능 추가

--- src/main/java/tryonu/api/config/WebClientConfig.java
+++ src/main/java/tryonu/api/config/WebClientConfig.java
@@ -5,13 +5,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
 
 /**
  * 외부 API 연동(WebClient) 목적별 Bean 등록
  * - 가상피팅
  * - 배경 제거
  */
+@Slf4j
 @Configuration
 public class WebClientConfig {
 
@@ -31,6 +35,8 @@ public class WebClientConfig {
     public WebClient fittingWebClient() {
         return WebClient.builder()
                 .baseUrl(fittingApiBaseUrl)
+                .filter(logRequest())
+                .filter(logResponse())
                 .build();
     }
 
@@ -41,6 +47,8 @@ public class WebClientConfig {
     public WebClient backgroundRemovalWebClient() {
         return WebClient.builder()
                 .baseUrl(backgroundRemovalApiBaseUrl)
+                .filter(logRequest())
+                .filter(logResponse())
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.MULTIPART_FORM_DATA_VALUE)
                 .defaultHeader(HttpHeaders.ACCEPT, MediaType.IMAGE_PNG_VALUE)
                 .build();
@@ -53,8 +61,27 @@ public class WebClientConfig {
     public WebClient categoryPredictionWebClient() {
         return WebClient.builder()
                 .baseUrl(categoryPredictionApiBaseUrl)
+                .filter(logRequest())
+                .filter(logResponse())
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.MULTIPART_FORM_DATA_VALUE)
                 .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
                 .build();
     }
+
+
+
+    private ExchangeFilterFunction logRequest() {
+        return ExchangeFilterFunction.ofRequestProcessor(request -> {
+            log.debug("Request: {} {}", request.method(), request.url());
+            return Mono.just(request);
+        });
+    }
+
+    private ExchangeFilterFunction logResponse() {
+        return ExchangeFilterFunction.ofResponseProcessor(response -> {
+            log.debug("Response status: {}", response.statusCode());
+            return Mono.just(response);
+        });
+    }
+
 } 
