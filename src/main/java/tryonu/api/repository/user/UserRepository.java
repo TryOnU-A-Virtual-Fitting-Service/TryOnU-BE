43a1c3245cf44d580469058be4884e55a8535520
givemethatsewon <nvpz1598@gmail.com>
2025-08-30 15:52:21 +0900

    feat: 사용자 UUID 조회에 비관적 락 기능 repository 레이어로 분리
    - lock을 거는 uuid 조회와 그렇지 않은 uuid 조회 쿼리로 분리하여 성능 최적화
    - user/init (레이스 컨디션 발생했던 엔드포인트)에선 비관적 락 적용 -> 레포지토리 레이어로 책임 위임

--- src/main/java/tryonu/api/repository/user/UserRepository.java
+++ src/main/java/tryonu/api/repository/user/UserRepository.java
@@ -1,40 +1,44 @@
 package tryonu.api.repository.user;
 
 import org.springframework.lang.NonNull;
-import tryonu.api.domain.User;  
+import tryonu.api.domain.User;
 import java.util.Optional;
 
-
 public interface UserRepository {
-    
+
     /**
      * 사용자 저장 (로깅 포함)
      */
     User save(@NonNull User user);
-    
+
     /**
      * 사용자 ID로 조회 (예외처리 포함)
      */
     User findByIdAndIsDeletedFalseOrThrow(@NonNull Long userId);
 
     /**
-     * 디바이스 ID로 사용자 조회
+     * 디바이스 ID로 사용자 조회 (일반 조회)
      */
     Optional<User> findByUuid(@NonNull String uuid);
-    
+
+    /**
+     * 디바이스 ID로 사용자 조회 (비관적 락 적용)
+     */
+    Optional<User> findByUuidWithLock(@NonNull String uuid);
+
     /**
      * 디바이스 ID로 사용자 조회 (예외처리 포함)
      */
     User findByUuidAndIsDeletedFalseOrThrow(@NonNull String uuid);
-    
+
     /**
      * 디바이스 ID로 사용자 존재 여부 확인
      */
     boolean existsByUuidAndIsDeletedFalse(@NonNull String uuid);
-    
+
     /**
      * 사용자 소프트 삭제 (예외처리 포함)
      */
     void softDelete(@NonNull User user);
-    
-} 
+
+}
